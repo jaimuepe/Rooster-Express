7e73e74cdba1f0793b1fdcc0f82508318cda5a64
Alejandro López <aloro2392@gmail.com>
2018-08-12 13:33:49 +0200

    Lusesicas de llegada
    Lusesicas de llegada

--- Assets/Scripts/DeliveryLogic.cs
+++ Assets/Scripts/DeliveryLogic.cs
@@ -24,8 +24,47 @@ private void OnTriggerEnter(Collider other)
             Destroy(other.gameObject);
             if(state.Equals(other.gameObject.GetComponent<Caja>().code)) {
                 gameManager.incrementPoints((float)System.Math.Round(other.GetComponent<Caja>().points, 2));
+                switch(state) {
+                    case "A":
+                        FindObjectOfType<LightsManager>().TurnGreen(0);
+                        break;
+                    case "B":
+                        FindObjectOfType<LightsManager>().TurnGreen(1);
+                        break;
+                    case "C":
+                        FindObjectOfType<LightsManager>().TurnGreen(2);
+                        break;
+                    case "D":
+                        FindObjectOfType<LightsManager>().TurnGreen(3);
+                        break;
+                    case "E":
+                        FindObjectOfType<LightsManager>().TurnGreen(4);
+                        break;
+                    default:
+                        break;
+                }
+
             } else {
                 gameManager.decrementPoints((float)System.Math.Round(other.GetComponent<Caja>().points, 2));
+                switch(state) {
+                    case "A":
+                        FindObjectOfType<LightsManager>().TurnRed(0);
+                    break;
+                    case "B":
+                        FindObjectOfType<LightsManager>().TurnRed(1);
+                    break;
+                    case "C":
+                        FindObjectOfType<LightsManager>().TurnRed(2);
+                    break;
+                    case "D":
+                        FindObjectOfType<LightsManager>().TurnRed(3);
+                    break;
+                    case "E":
+                        FindObjectOfType<LightsManager>().TurnRed(4);
+                    break;
+                    default:
+                        break;
+                }
             }
         }
     }
