4fc6f37ff84bdfd1e26a9e728c8841e9e9794b3a
jaimuepe <jaimuepe@gmail.com>
2018-08-12 15:38:29 +0200

    controles mando

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public Canvas tutorialCanvas;
+
+    void Start()
+    {
+        // TODO
+    }
+}
