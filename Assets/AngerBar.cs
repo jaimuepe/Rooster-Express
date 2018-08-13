e23b9261ccfb50a707671719e1db1c819acf75a1
jaimuepe <jaimuepe@gmail.com>
2018-08-13 19:36:36 +0200

    anger bar

--- Assets/AngerBar.cs
+++ Assets/AngerBar.cs
@@ -6,20 +6,46 @@ using UnityEngine.UI;
 [ExecuteInEditMode]
 public class AngerBar : MonoBehaviour
 {
-    public Image[] chickenImages;
+    public Image chicken;
+    public Sprite[] chickenSprites;
     public float concernedChickenValue;
     public float angryChickenValue;
 
     [Range(0.0f, 1.0f)]
     public float angerValue;
 
+    [SerializeField]
+    float maxHeight;
+    RectTransform rectTransform;
+    RectTransform chickenRectTransform;
+
     void Start()
     {
-
+        rectTransform = GetComponent<RectTransform>();
+        chickenRectTransform = chicken.GetComponent<RectTransform>();
     }
 
     void Update()
     {
+#if UNITY_EDITOR
+        if (chickenRectTransform == null) { chickenRectTransform = chicken.GetComponent<RectTransform>(); }
+#endif
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, angerValue * maxHeight);
+        if (angerValue < concernedChickenValue)
+        {
+            chicken.sprite = chickenSprites[0];
+        }
+        else if (angerValue < angryChickenValue)
+        {
+            chicken.sprite = chickenSprites[1];
+        }
+        else
+        {
+            chicken.sprite = chickenSprites[2];
+        }
 
+        chickenRectTransform.anchoredPosition = new Vector2(
+            chickenRectTransform.anchoredPosition.x,
+            rectTransform.anchoredPosition.y + angerValue * maxHeight);
     }
 }
